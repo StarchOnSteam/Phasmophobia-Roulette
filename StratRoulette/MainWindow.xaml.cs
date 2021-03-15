d5c3269771f2fe9e5348eebfeb2df16fc4ff3ae5
StarchOnSteam <46310750+StarchOnSteam@users.noreply.github.com>
2021-03-15 13:38:11 +0800

    Update MainWindow.xaml.cs

--- StratRoulette/MainWindow.xaml.cs
+++ StratRoulette/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2021 Riley Mullett
+Copyright (c) 2021 StarchOnSteam
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
